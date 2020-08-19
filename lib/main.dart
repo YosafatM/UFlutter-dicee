f8d3e3f4fd72c3f7e397ed83e700c7ef96d345d6
YosafatM <yosafat_martinez21@hotmail.com>
2020-08-19 16:05:50 -0500

    Subida del proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,15 +11,59 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: PageDice(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class PageDice extends StatefulWidget {
+  @override
+  _PageDiceState createState() => _PageDiceState();
+}
+
+class _PageDiceState extends State<PageDice> {
+  int leftNumber = 1;
+  int rightNumber = 1;
+
+  void changeDice() {
+    leftNumber = Random().nextInt(6) + 1;
+    rightNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDice();
+                  });
+                },
+                child: Image.asset('images/dice$leftNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDice();
+                  });
+                },
+                child: Image.asset('images/dice$rightNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
